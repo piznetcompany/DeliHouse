56a8a4c89112f9535f93fc8fcdd5268cace325d9
mwc <mwc@netcompany.com>
2016-10-12 09:49:49 +0200

    Initial project structure

---
+++ EiTSystem/EiTFrontend/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using EiTFrontend.Infrastructure;
+
+namespace EiTFrontend.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
